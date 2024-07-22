79996cc448750245d2060cacd20fcd8f1c72f86b
JianWei-0510 <ss41210510@gmail.com>
2024-07-22 13:38:50 +0800

    add delete post e2e testing

--- tests/post.spec.ts
+++ tests/post.spec.ts
@@ -1,16 +1,16 @@
 import { test, expect } from "@playwright/test";
 
-test.beforeEach(async ({ page }) => {
+test.beforeEach(async ({ page }, testInfo) => {
+  testInfo.setTimeout(testInfo.timeout + 10000);
+
   await page.goto("/");
   await expect(page).toHaveTitle("GitPost");
+  await page.getByRole("link", { name: "Login" }).click();
+  await page.waitForURL("/Jian-dog");
 });
 
 test.describe("Post Manipulation", () => {
   test("should able to create post", async ({ page }) => {
-    await page.getByRole("link", { name: "Login" }).click();
-
-    await page.waitForURL("/Jian-dog");
-
     await page.getByRole("link", { name: "post", exact: true }).click();
 
     await page.getByPlaceholder("Title").fill("playwright E2E posting test");
@@ -32,5 +32,17 @@ test.describe("Post Manipulation", () => {
         name: "This is a dummy post created by playwright testing framework.",
       })
       .isVisible();
+  });
+
+  test("should able to delete first latest post", async ({ page }) => {
+    await page.locator(".p-6").first().click();
+    await page
+      .locator("div")
+      .filter({ hasText: /^Jian-dog$/ })
+      .getByRole("button")
+      .click();
+    await page.getByRole("link", { name: "Delete" }).click();
+    await page.getByRole("button", { name: "Delete" }).click();
+    await page.goto("http://localhost:3000/Jian-dog");
   });
 });
